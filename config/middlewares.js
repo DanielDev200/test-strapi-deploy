aaf535bfefb009aa45aafe94600a49f48ddfc583
Daniel Serrano <51222713+DanielDev200@users.noreply.github.com>
2024-04-09 08:49:41 -0700

    added a blog

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,16 @@
+const { env } = require('@strapi/utils');
+
 module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
+  // {
+  //   name: 'strapi::cors',
+  //   config: {
+  //     origin: env('CORS_ORIGIN').split(',')
+  //   },
+  // },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
